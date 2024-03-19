761521e5ef5bdd75d0c963b6635a6ceb5f429f30
alexandrecr <devalexandrecr@gmail.com>
2024-03-18 20:40:20 -0500

    feat: add new dataStore

--- emulators/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "Start Emulators.."
-cd /home/node/firebase/admin || exit
-npm run start:emulators
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 # This script checks if the container is started for the first time.
 
 CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
-if [ ! -e /home/node/apps/$CONTAINER_FIRST_STARTUP ]; then
+if [ ! -e /root/apps/$CONTAINER_FIRST_STARTUP ]; then
     echo "installing apps..."
     rm -rf functions || exit && rm -rf api || exit && rm -rf admin || exit && \
     git clone git@github.com:DevAlexandreCR/gorda-functions.git functions && \
@@ -15,7 +15,7 @@ if [ ! -e /home/node/apps/$CONTAINER_FIRST_STARTUP ]; then
 
     echo "installing functions..."
 
-    cd /home/node/apps/functions || exit
+    cd /root/apps/functions || exit
     npm cache clean -force && \
     git checkout develop && npm install --no-interaction && \
     npm run build
@@ -25,7 +25,7 @@ if [ ! -e /home/node/apps/$CONTAINER_FIRST_STARTUP ]; then
     #######################################################
 
     echo "installing Api..."
-    cd /home/node/apps/api || exit
+    cd /root/apps/api || exit
     git checkout develop && \
     cp .env.example .env && cp .env.example .env.testing && \
     npm install --no-interaction && \
@@ -36,19 +36,20 @@ if [ ! -e /home/node/apps/$CONTAINER_FIRST_STARTUP ]; then
     #######################################################
 
     echo "installing Admin..."
-    cd /home/node/apps/admin || exit
+    cd /root/apps/admin || exit
 
     git checkout develop && \
     cp .env.example .env.local && cp .env.example .env.testing && cp .env.example .env.production && \
     cp firebase.example.json firebase.json && \
     npm install --no-interaction && \
     sed -i 's/"host": "127.0.0.1"/"host": "0.0.0.0"/' firebase.json && \
-    cp -r /home/node/dataEmulators dataEmulators && \
-    touch /home/node/apps/$CONTAINER_FIRST_STARTUP && \
+    cp -r /root/dataEmulators dataEmulators && \
+    touch /root/apps/$CONTAINER_FIRST_STARTUP && \
     echo "installation ended"
 
 else
   echo "Apps running..."
-  cd /home/node/apps/admin || exit
+  cp -r /root/dataEmulators dataEmulators
+  cd /root/apps/admin || exit
   tail -f /dev/null
 fi
